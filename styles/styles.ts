ade7a65483b8a4f94c00d6da09d3fdee5b1fa8da
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-13 10:24:15 -0400

    rewrote dynamic styles in tailwind

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,67 +1,13 @@
 import { StyleSheet } from 'react-native';
 import tw from "twrnc";
 
-  /**
-   * 
-   * This component returns a different stylesheet, based on dark mode.
-   * 
-  * Format:
-  * 
-  * StyleSheet.create({
-  *   componentName: darkMode ? darkModeStyles : lightModeStyles,
-  * })
-  */
- // TODO: Rewrite all styles with tailwindcss
-  export const getDynamicStyles = (darkMode : boolean) => StyleSheet.create(
-    {
-    textStyle: darkMode ? tw`text-white` : tw`text-gray-700`,
 
-    containerStyle: darkMode ? {
-      flex: 1,
-      backgroundColor: "#121212",
-      padding: 20,
-    } : {
-      flex: 1,
-      backgroundColor: "#f0f0f0",
-      padding: 20,
-    },
-
-    summaryCardStyle: darkMode ? {
-      borderWidth: 1,
-      borderColor: "#00bcd4",
-      borderRadius: 15,
-      padding: 25,
-      backgroundColor: "#333333",
-      marginBottom: 15,
-    } : {
-      borderWidth: 1,
-      borderColor: "#00bcd4",
-      borderRadius: 15,
-      padding: 25,
-      backgroundColor: "#ffffff",
-      marginBottom: 15,
-    },
-
-    borderCardStyle: darkMode ? {
-      borderWidth: 1,
-      borderColor: "#383737",
-      borderRadius: 15,
-      padding: 25,
-      width: "48%",
-      alignItems: "center",
-      backgroundColor: "#444444",
-      marginBottom: 15,
-    } : {
-      borderWidth: 1,
-      borderColor: "#383737",
-      borderRadius: 15,
-      padding: 25,
-      width: "48%",
-      alignItems: "center",
-      backgroundColor: "#f7f7f7",
-      marginBottom: 15,
-    },
-    actionButton: 
-      tw`${darkMode ? "bg-gray-700" : "bg-gray-300"} flex-1 mx-2 py-3 p-4 rounded-lg items-center`,
-    
+//This component returns a different stylesheet, based on dark mode.
+export const getDynamicStyles = (darkMode: boolean) => StyleSheet.create(
+  {
+    textStyle: tw`${darkMode ? "text-white" : "text-gray-700"}`,
+    containerStyle: tw`${darkMode ? "bg-gray-800" : "bg-gray-200"} flex-1 p-5`,
+    summaryCardStyle: tw`${darkMode ? "bg-gray-800" : "bg-white"} border border-cyan-500 rounded-2xl p-6 mb-4`,
+    borderCardStyle: tw`${darkMode ? "bg-gray-800" : "bg-gray-200"} flex-1 mx-2 p-6 border border-gray-700 rounded-2xl items-center mb-4`,
+    actionButton: tw`${darkMode ? "bg-gray-700" : "bg-gray-300"} flex-1 mx-2 py-3 p-4 rounded-lg items-center`,
   })
