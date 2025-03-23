85d1b9e581de6c01fe06a7a6770ba7dcac7c26fc
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-23 13:36:17 -0400

    remove function that was moved to different file

--- services/itemService.ts
+++ services/itemService.ts
@@ -97,35 +97,6 @@ export const fetchItemsByFolder = async (): Promise<{ folders: string[]; itemsBy
   }
 };
 
-//Returns an object with only fields that have changed (excluding the id)
-function getChangedFields(
-  oldItem: Item,
-  newItem: Item
-): Partial<Omit<Item, 'id'>> {
-
-  //Store changes in a partial item object (excluding id)
-  const changes: Partial<Omit<Item, 'id'>> = {};
-
-  //Get the keys of the new item object
-  const keys = Object.keys(newItem) as (keyof Item)[];
-
-  //For each key of the item object
-  for (const key of keys) {
-    //omit id for the purposes of changes
-    if (key === 'id') continue;
-
-    const oldValue = oldItem[key];
-    const newValue = newItem[key];
-
-    //when values don't match, store in changes
-    if (oldValue !== newValue) {
-      changes[key as keyof Omit<Item, 'id'>] = newValue as any;
-    }
-  }
-
-  return changes;
-}
-
 export const editItem = async (oldItem: Item, newItem: Item): Promise<boolean> => {
 
     if (oldItem.id !== newItem.id) {
