560e79852946d3aa55dbb2121dd0faf2843f0262
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-27 13:50:35 -0400

    Alert message when redirect while signed in

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,13 @@
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Alert } from "react-native";
 
 export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth();
 
   if (isSignedIn) {
     //redirect to homepage if signed in
+    Alert.alert("You are signed in!.");
     return <Redirect href={"/(tabs)/dashboard"} />;
   }
 
