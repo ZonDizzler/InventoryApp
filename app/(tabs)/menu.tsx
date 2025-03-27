af2d7b7f302d78523bfe4fb7b5e3149ac39c0c73
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-27 10:18:25 -0400

    implement real signoutbutton, +vscode changes

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, Switch, TouchableOpacity, StyleSheet } from "react-native";
 import { useTheme } from "@darkModeContext";
 import { Link, router } from "expo-router";
+import SignOutButton from "@/components/SignOutButton";
 
 export default function Menu() {
   const { darkMode, toggleDarkMode } = useTheme();
@@ -42,14 +43,14 @@ export default function Menu() {
       </TouchableOpacity>
 
       <View style={styles.card}>
-  <Text style={styles.flexText}>Notifications</Text>
-  <Switch
-    value={notificationsEnabled}
-    onValueChange={setNotificationsEnabled}
-    trackColor={{ false: "#ccc", true: "#00bcd4" }}
-    thumbColor={notificationsEnabled ? "#00bcd4" : "#f4f3f4"}
-  />
-</View>
+        <Text style={styles.flexText}>Notifications</Text>
+        <Switch
+          value={notificationsEnabled}
+          onValueChange={setNotificationsEnabled}
+          trackColor={{ false: "#ccc", true: "#00bcd4" }}
+          thumbColor={notificationsEnabled ? "#00bcd4" : "#f4f3f4"}
+        />
+      </View>
       <View style={styles.card}>
         <Text style={styles.flexText}>Display</Text>
         <View style={styles.row}>
@@ -64,9 +65,7 @@ export default function Menu() {
           />
         </View>
       </View>
-      <Link href="/" style={styles.signOutButton}>
-        <Text style={styles.signOutButtonText}>Sign Out</Text>
-      </Link>
+      <SignOutButton />
     </View>
   );
 }
@@ -77,7 +76,7 @@ const getStyles = (theme: string) => {
   return StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: isDarkMode ? '#1F2937' : '#f5f5f5', // Updated dark mode background
+      backgroundColor: isDarkMode ? "#1F2937" : "#f5f5f5", // Updated dark mode background
       padding: 20,
     },
     headerText: {
@@ -99,9 +98,9 @@ const getStyles = (theme: string) => {
       color: "white",
     },
     profileCard: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: isDarkMode ? '#374151' : '#ffffff', // Slightly lighter dark gray for contrast
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: isDarkMode ? "#374151" : "#ffffff", // Slightly lighter dark gray for contrast
       padding: 15,
       borderRadius: 10,
       marginBottom: 10,
@@ -111,7 +110,7 @@ const getStyles = (theme: string) => {
       fontWeight: "bold",
     },
     card: {
-      backgroundColor: isDarkMode ? '#374151' : '#ffffff', // Match profile card in dark mode
+      backgroundColor: isDarkMode ? "#374151" : "#ffffff", // Match profile card in dark mode
       padding: 15,
       borderRadius: 10,
       marginBottom: 10,
