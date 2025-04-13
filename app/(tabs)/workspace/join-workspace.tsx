a4ffe4fd612bb5b711eb84a89a5ad361d2131483
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-04-13 12:48:57 -0400

    accept invite

--- app/(tabs)/workspace/join-workspace.tsx
+++ app/(tabs)/workspace/join-workspace.tsx
@@ -1,11 +1,73 @@
 import { Link } from "expo-router";
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
+  FlatList,
+} from "react-native";
 import tw from "twrnc";
 import { useTheme } from "@darkModeContext";
+import { useOrganizationList, useUser } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function JoinWorkspace() {
   const { darkMode } = useTheme();
+
+  // https://clerk.com/docs/hooks/use-organization-list
+  const { isLoaded, userInvitations } = useOrganizationList({
+    userInvitations: {
+      // Set pagination parameters
+      infinite: true,
+    },
+  });
+
+  //The current user
+  const { user } = useUser();
+
+  if (!user) {
+    return <Text>You aren't signed in</Text>;
+  }
+
+  if (!isLoaded || userInvitations.isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  const renderUserInvite = ({ item }: any) => {
+    return (
+      <View style={styles.contributor}>
+        <Text>{item.emailAddress}</Text>
+        <Text>{item.publicOrganizationData.name}</Text>
+        <Text>{item.role}</Text>
+        <TouchableOpacity
+          onPress={async () => {
+            try {
+              await item.accept();
+
+              userInvitations.revalidate();
+
+              Alert.alert(
+                "Success",
+                `Accepted ${item.publicOrganizationData.name}'s invite`
+              );
+            } catch (error: any) {
+              Alert.alert("Error", error.message || "Something went wrong");
+            }
+          }}
+        >
+          <Ionicons name="checkbox-outline" size={20} color="green" />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   const backgroundColor = darkMode ? "#1F2937" : "#ffffff";
   const textColor = darkMode ? "#ffffff" : "#000000";
@@ -27,14 +89,19 @@ export default function JoinWorkspace() {
           </Link>
         </TouchableOpacity>
       </View>
-      <Text style={[styles.title, { color: textColor }]}>
-        Enter Invite Code
-      </Text>
-      <View style={styles.codeContainer}></View>
-      <Text style={[styles.infoText, { color: infoTextColor }]}>
-        In case you have no code or it's expired, please request a new code from
-        the workspace owner.
-      </Text>
+      {/* User Invitation List */}
+      {userInvitations.data && userInvitations.data.length > 0 && (
+        <>
+          <Text style={[styles.title, darkMode && { color: "white" }]}>
+            Invitations
+          </Text>
+          <FlatList
+            data={userInvitations.data}
+            keyExtractor={(item: any) => item.id}
+            renderItem={renderUserInvite}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -71,5 +138,19 @@ const styles = StyleSheet.create({
   },
   infoText: {
     color: "#888",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contributor: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    marginBottom: 5,
   },
 });
